b8801403df1213acf1379e6af1a341ac9b76de0e
hagi845 <89743047+hagi845@users.noreply.github.com>
2023-01-06 19:18:07 +0900

    edit

---
+++ DisplayActionSheetBuilderSample/DisplayActionSheetBuilderSample/EditPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DisplayActionSheetBuilderSample
+{
+    public partial class EditPage : ContentPage
+    {
+        public EditPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
--- DisplayActionSheetBuilderSample/DisplayActionSheetBuilderSample/MainPage.xaml.cs
+++ DisplayActionSheetBuilderSample/DisplayActionSheetBuilderSample/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using static System.Collections.Specialized.BitVector32;
 
 namespace DisplayActionSheetBuilderSample
 {
@@ -17,32 +19,93 @@ namespace DisplayActionSheetBuilderSample
 
         async void Button_Clicked(System.Object sender, System.EventArgs e)
         {
-            const string EDIT_CAPTION = "編集する";
-            const string DELETE_CAPTION = "削除する";
-            const string CANCEL_CAPTION = "閉じる";
-
-            var buttons = new string[] { EDIT_CAPTION, DELETE_CAPTION };
-
-            var selectedText = await this.DisplayActionSheet(null, CANCEL_CAPTION, null, buttons);
-
-            if (selectedText == CANCEL_CAPTION) return;
-
-            if (selectedText == EDIT_CAPTION)
-            {
-
-                await this.DisplayAlert("移動", "コメント編集 のページへ移動します。(未実装)", "OK");
-                return;
-            }
-
-            if (selectedText == DELETE_CAPTION)
-            {
-                var result = await this.DisplayAlert("コメントを削除", "本当にこのコメントを削除してよろしいですか？", "削除", "キャンセル");
-                if (!result) return;
-
-
-                return;
-            }
+            await new DisplayActionSheetDirector(new DisplayActionSheetFuncBuilder(this)).ShowAsync();
         }
     }
+
+    public interface IBuilder
+    {
+        IBuilder AddSelectionButton(string title, Action<Page> action);
+        IBuilder SetCancelButton(string title, Action<Page> action);
+        Func<Task> Create();
+    }
+
+    public class DisplayActionSheetFuncBuilder : IBuilder
+    {
+        Page _page;
+        Dictionary<string, Action<Page>> buffer;
+        KeyValuePair<string, Action<Page>> _cancelBuffer;
+
+
+        public DisplayActionSheetFuncBuilder(Page page)
+        {
+            // 引数でvmを受け取る想定
+
+            _page = page;
+            buffer = new Dictionary<string, Action<Page>>();
+            _cancelBuffer = new KeyValuePair<string, Action<Page>>("閉じる", (p) => { });
+        }
+
+        public IBuilder AddSelectionButton(string title, Action<Page> action)
+        {
+            buffer[title] = action;
+            return this;
+        }
+
+        public IBuilder SetCancelButton(string title, Action<Page> action)
+        {
+            _cancelBuffer = new KeyValuePair<string, Action<Page>>(title, action);
+            return this;
+        }
+
+        public Func<Task> Create()
+        {
+            Func<Task> f = async () =>
+            {
+                var resp = await _page.DisplayActionSheet(null, _cancelBuffer.Key, null, GetButtons());
+                if (resp == _cancelBuffer.Key)
+                {
+                    _cancelBuffer.Value(_page);
+                    return;
+                }
+
+                buffer.First(x => x.Key == resp).Value(_page);
+            };
+
+            return f;
+        }
+
+        string[] GetButtons() => buffer.Keys.ToArray();
+    }
+
+    public class DisplayActionSheetDirector
+    {
+        protected IBuilder _builder;
+
+        public DisplayActionSheetDirector(IBuilder builder)
+        {
+            // 処理にvmが必要な場合はコンストラクタで受け取る
+
+            _builder = builder;
+        }
+
+        public async Task ShowAsync()
+        {
+            _builder.AddSelectionButton("編集", (p) =>
+            {
+                p.Navigation.PushAsync(new EditPage());
+                Console.WriteLine("edit");
+            });
+
+            _builder.AddSelectionButton("削除", (p) =>
+            {
+                Console.WriteLine("delete");
+            });
+
+            var func = _builder.Create();
+            await func.Invoke();
+        }
+    }
+
 }
 
